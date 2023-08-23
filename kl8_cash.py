0f4968e14ecaeeaa954bdc36a939ee0bce5499e6
KittenCN <owdely@vip.163.com>
2023-08-23 20:51:25 +0800

    fix bugs

--- kl8_cash.py
+++ kl8_cash.py
@@ -1,11 +1,6 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import random
-from tqdm import tqdm
-from sklearn.cluster import KMeans
-from collections import defaultdict
 from config import *
-from common import get_data_run, datetime
+from common import get_data_run
 from itertools import combinations
 
 name = "kl8"
@@ -16,16 +11,17 @@ cash_select = [10, 9, 8, 7, 6 ,5 ,0]
 cash_price = [5000000, 8000, 800, 80, 5, 3, 2]
 cash_list = [0] * len(cash_select)
 
-cash_file_name = "result_20230823120804.csv"
+cash_file_name = "result_20230823162832.csv"
 cash_data = pd.read_csv(cash_file_name)
 cash_numpy = cash_data.to_numpy()
 
-for index in  range(len(cash_select)):
-    for item in cash_numpy:
+for item in cash_numpy:
+    cash_0 = set(item) & set(ori_numpy)
+    if len(cash_0) == 0:
+        cash_list[-1] += 1
+    for index in  range(len(cash_select) - 1):
         ori_split = list(combinations(ori_numpy, cash_select[index]))
-        cash_split = list(combinations(item, cash_select[index]))
-
-        cash_set = set(ori_split) & set(cash_split)
+        cash_set = set(ori_split) & set(item)
         cash_list[index] += len(cash_set)
 
 for i in range(len(cash_select)):
